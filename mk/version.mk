537ed4fb5f77cadad90ccdd4506f77e7e07db1c2
Dmitry Ponyatov <dponyatov@gmail.com>
2025-09-15 14:09:54 +0400

    Signed-off-by: Dmitry Ponyatov <dponyatov@gmail.com>

---
+++ mk/version.mk
@@ -0,0 +1,17 @@
+# Cortex-M
+NEWLIB_VER   = 3.3.0
+CUBEMX_VER   = 6.12.1
+
+# cross-Linux SDK
+## GCC toolchain
+GMP_VER      = 6.3.0
+MPFR_VER     = 4.2.2
+MPC_VER      = 1.3.1
+BINUTILS_VER = 2.45
+GCC_VER      = 15.2.0
+GDB_VER      = 16.3
+## embedded Linux
+# LINUX_VER  = 6.12.43
+LINUX_VER    = 6.16.3
+UCLIBC_VER   = 1.0.54
+BB_VER       = 1_36_1
