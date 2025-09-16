978fafb53d5f481a913b5c804a6555f89d75bf50
Dmitry Ponyatov <dponyatov@gmail.com>
2025-09-16 11:00:46 +0400

    Signed-off-by: Dmitry Ponyatov <dponyatov@gmail.com>

--- cpu/inc/cpu.hpp
+++ cpu/inc/cpu.hpp
@@ -1,10 +1,2 @@
 /// @defgroup cpu cpu
 /// @ingroup cross
-/// @defgroup cpux86 x86
-/// @ingroup cpu
-/// @defgroup cpurpi rpi
-/// @ingroup cpu
-/// @defgroup cpucm cm
-/// @ingroup cpu
-/// @defgroup cpuesp esp
-/// @ingroup cpu
