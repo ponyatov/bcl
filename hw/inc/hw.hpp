679f6081fc4770fa9e191c9fbaa8449f9eaf3b98
Dmitry Ponyatov <dponyatov@gmail.com>
2025-09-16 09:54:46 +0400

    Signed-off-by: Dmitry Ponyatov <dponyatov@gmail.com>

--- hw/inc/hw.hpp
+++ hw/inc/hw.hpp
@@ -1,3 +1,11 @@
 /// @defgroup cross cross
 /// @defgroup hw hw
 /// @ingroup cross
+/// @defgroup hwx86 hwx86
+/// @ingroup hw
+/// @defgroup hwrpi hwrpi
+/// @ingroup hw
+/// @defgroup hwcm hwcm
+/// @ingroup hw
+/// @defgroup hwesp hwesp
+/// @ingroup hw
